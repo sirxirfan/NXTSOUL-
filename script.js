4b95f6e0b26c4b5dcef01ca022107415a4eca5da
sirxirfan <uzirfan780@gmail.com>
2026-06-10 21:00:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const botLink = "https://t.me/quickagebot";
+
+document.getElementById("mainBtn").onclick = () => {
+    window.location.href = botLink;
+};
+
+document.getElementById("phoneBtn").onclick = () => {
+    window.location.href = botLink;
+};
+
+document.getElementById("navBtn").onclick = () => {
+    window.location.href = botLink;
+};
